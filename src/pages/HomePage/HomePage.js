1559f32853208cc2ad81862ce20d25d9716b7648
Lee Neufeld <leeneufeld23@hotmail.com>
2022-01-16 19:03:57 -0700

    init setup

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+
+function HomePage(){
+
+    return (
+    <Box>
+        <Typography>Home Page</Typography>
+    </Box>
+    );
+
+    }
+
+export default HomePage
